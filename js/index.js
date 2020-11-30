5d691992399eef7a61b6df7061f9d4dad8328d1e
Jaynee Salcedo <jsalcedoeducation@gmail.com>
2020-11-30 12:34:16 -1000

    Nav Links Text Complete

--- js/index.js
+++ js/index.js
@@ -36,55 +36,95 @@ const siteContent = {
     "copyright" : "Copyright Great Idea! 2018"
   },
 };
-
+//--------------------------------------------------------------------------------------------
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
 
-//Header Section------------------------------------------
 
+//Header Section-----------------------------------------------------------------------------
+
+const navLinks = document.querySelectorAll("nav a");
+// console.log(navLinks);
+
+const navLinksArray = Array.from(navLinks);
+// console.log(navLinksArray);
+
+//--------------------------------------------
   //Task: Add "Services" to nav a 1
+  const navService = document.querySelector('.container nav a');
+  navService.textContent = 'Services';
+  // console.log(navService);
 
+//--------------------------------------------
   //Task: Add "Product" to nav a 2
+  const navProduct = navService.nextElementSibling;
+  navProduct.textContent = 'Product';
+  // console.log(navProduct);
 
+//--------------------------------------------
   //Task: Add "Vision" to nav a 3
+  const navVision = navProduct.nextElementSibling;
+  navVision.textContent = 'Vision';
 
+//--------------------------------------------
   //Task: Add "Features" to nav a 4
+  const navFeatures = navVision.nextElementSibling;
+  navFeatures.textContent = 'Features';
 
+//--------------------------------------------
   //Task: Add "About" to nav a 5
+  const navAbout = navFeatures.nextElementSibling;
+  navAbout.textContent = 'About';
 
+//--------------------------------------------
   //Task: Add "Contact" to nav a 6
+  const navContact = navAbout.nextElementSibling;
+  navContact.textContent = 'Contact';
+  
+//Section Class cta-------------------------------------------------------------------------
 
-//Section Class cta---------------------------------------
-
+//--------------------------------------------
   //Task: Add DOM <br> Is <br> Awesome to <h1>
 
+//--------------------------------------------
   //Task: Add "Get Started" to Button
 
+//--------------------------------------------
   //Task: Add img src
 
-//Section Class main-content------------------------------
+//Section Class main-content---------------------------------------------------------------
 
+//--------------------------------------------
   //Task: top-content- h4 + p of Features
 
+//--------------------------------------------
   //Task: top-content- h4 + p of About
 
+//--------------------------------------------
   //Task: img src
 
+//--------------------------------------------
   //Task: bottom-content- h4 + p of Services
 
+//--------------------------------------------
   //Task: bottom-content- h4 + p of Product
 
+//--------------------------------------------
   //Task: bottom-content- h4 + p of Vision
 
-//Section Class contact------------------------------
+//Section Class contact-------------------------------------------------------------------
 
+//--------------------------------------------
   //Task: h4 "Contact"
 
+//--------------------------------------------
   //Task: p "123 Way 456 Street<br>Somewhere, USA"
 
+//--------------------------------------------
   //Task: p "1 (888) 888-8888"
 
+//--------------------------------------------
   //Task: p "sales@greatidea.io"
 
 //Footer---------------------------------------------
